1cb5367b2040087efd5385a5d561df42304c9c5f
amiral4492 <chaussenerys@gmail.com>
2025-03-21 15:47:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
-const cors = require('cors'); // Ajoutez cette ligne
+const cors = require('cors');
 const app = express();
+const path = require('path'); // Ajoutez cette ligne
 
-// Activer CORS pour toutes les routes
+// Servir les fichiers statiques (frontend)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Activer CORS
 app.use(cors());
 
 // Initialiser Supabase
-const supabaseUrl = 'https://bqywnyplxcyhcwefrsgl.supabase.co'; // Remplacez par votre URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxeXdueXBseGN5aGN3ZWZyc2dsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MDE4OTksImV4cCI6MjA1ODA3Nzg5OX0.8yz3OzTH0uJV_7Sd--vHnLecFL42b9GNnrT4POkF90E'; // Remplacez par votre clé API
+const supabaseUrl = 'https://votre-url.supabase.co'; // Remplacez par votre URL
+const supabaseKey = 'votre-cle-api'; // Remplacez par votre clé API
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(express.json());
